66ef1ee4ca6cf2e34f06a1c57d038d9818bacade
matsuzakitatsuya <tatsu.de50@gmail.com>
2018-03-14 10:28:57 +0000

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,32 @@
 class BlogsController < ApplicationController
-	def index
-	end
+  def new
+  	@blog = Blog.new
+  	#ブログモデルと紐づいた（データを入れるための）空の箱
+  	#インスタンスという
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	#ビューから贈られてきたデータをblogsという空き箱に入れている
+  	blog.save
+  	redirect_to "/blogs"
+  	#他のビューに移動するメソッド
+  end
+
+  def index
+  	@blogs = Blog.all
+  end
+
+  def show
+  	@blog = Blog.find(params[:id])
+  end
+
+  private
+
+	  def blog_params
+		  	params.require(:blog).permit(:title, :body, :category)
+		  	#ビューから送信されたデータはparamsというメゾットの中に入っている
+		  	#ストロングパラメータは、セキュリティを強固にしているもの
+	  end
+
 end
